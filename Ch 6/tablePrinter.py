37e517912df678c47d94780245fdc072bce6922b
Martin Verrascina <martinverrascina@gmail.com>
2020-05-02 02:35:25 +0200

    Edit tablePrinter.py (add comments)

--- Ch 6/tablePrinter.py
+++ Ch 6/tablePrinter.py
@@ -1,11 +1,16 @@
-#Table Printer CH6
-
+# AutomateTheBoringStuff Ch6: Table Printer
+"""
+Write a function named printTable() that takes a list of lists of strings
+and displays it in a well-organized table with each column right-justified.
+Assume that all the inner lists will contain the same number of strings.
+"""
 def printTable(toOrder):
     cols=len(toOrder[0])
     rows = len(toOrder)
     lenEachCol=[]
     maxTemp = 0
-    
+
+    # get lenght of the longest word in each column
     for c in range(cols):
         for r in range(rows):
             if len(toOrder[r][c])>maxTemp:
@@ -13,6 +18,7 @@ def printTable(toOrder):
         lenEachCol.append(maxTemp)
         maxTemp=0
 
+    # print
     for r in range(rows):
         for c in range(cols):
             print(toOrder[r][c].ljust(lenEachCol[c]+1), end="")
