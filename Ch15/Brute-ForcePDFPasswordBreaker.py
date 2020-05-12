51940bab1b0deaad0a87f533791fc67b8531aa77
martinverr <martinverrascina@gmail.com>
2020-05-13 00:25:26 +0200

    Update Brute-ForcePDFPasswordBreaker.py (Edited print messaged)

--- Ch15/Brute-ForcePDFPasswordBreaker.py
+++ Ch15/Brute-ForcePDFPasswordBreaker.py
@@ -4,7 +4,7 @@ import PyPDF2
 import os
 import sys
 
-filename = input("Insert a valid path to the pdf file")
+filename = input("Insert a valid path to the pdf file: ")
 if not os.path.exists(filename):
     print("Err: Not a valid path(filename)")
     sys.exit()
@@ -15,13 +15,13 @@ passwFound = None
 count = 0
 pdfReader = PyPDF2.PdfFileReader(PdfFile)
 
+print("<words checked> / <words in the dictionary> (<rate>) | <current word>")
 # Bruteforcing words in DictFile to decrypt pdfReader
 for word in DictFile:
     word = word.strip()
 
-    count += 1
     if count % 500 == 0:
-        print(f"words checked: {count} / 44000 ({count/44000*100}%) | {word}")
+        print(f"{count} / 88000 ({count/88000*100}%) | {word}")
 
     if pdfReader.decrypt(word):
         passwFound = word
@@ -29,6 +29,7 @@ for word in DictFile:
     elif pdfReader.decrypt(word.lower()):
         passwFound = word.lower()
         break
+    count += 2
 
 # result
 print()
